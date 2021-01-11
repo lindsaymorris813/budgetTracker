17a73ff78fcc7553626361fb4977742bdc2abeee
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-10 22:58:58 -0500

    type on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "styles.css.png"
+    "styles.css"
 ]
 
 const CACHE_NAME = "static-cache-v2";
